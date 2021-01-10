6fe48dabe2f98031c79d4877e1b3f1bffe6140c6
walaam126 <walaamajeed12@gmail.com>
2021-01-10 15:18:59 +0300

    conditionals solved

--- calculator.js
+++ calculator.js
@@ -1,3 +1,17 @@
 // Define the numbers and the operation here
-
+const num1=prompt("Enter first number");
+const num2=prompt("Enter second number");
+const opt=prompt("Enter the operation");
 // Write if-statements here
+if (opt==="+"){
+console.log(`Result: ${num1+num2}`);
+}
+else if (opt==="-"){
+console.log(`Result: ${num1-num2}`);
+}
+else if (opt==="*"){
+console.log(`Result: ${num1*num2}`);
+}
+else {
+console.log(`Result: ${num1/num2}`);
+}
